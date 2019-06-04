7fd21f5eeaed02ec0d13392f6a14d5631c86976a
Ashutoshh <asashuthegreat4@gmail.com>
2019-06-04 12:42:34 +0530

    Add files via upload
    this is day 2 just trail

---
+++ day2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import  time
+#  loading time module / library 
+x=10
+y=3.4
+
+s="hello world"
+
+print("hello world this is python3")
+time.sleep(3)  #  3 sec delay
+print(type(x))
